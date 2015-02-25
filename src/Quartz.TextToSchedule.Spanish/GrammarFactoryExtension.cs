4fcff8940d3047121f5d306a753730abf61fc734
Byron Roman <bromanv@gmail.com>
2015-02-25 08:33:51 -0600

    Sort code
    move some files to it correct path

---
+++ src/Quartz.TextToSchedule.Spanish/GrammarFactoryExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quartz.TextToSchedule.Grammars
+{
+    public static class SpanishGrammarFactoryExtension
+    {
+        public static ITextToSchedule CreateSpanishParser(this TextToScheduleFactory factory)
+        {
+            return new TextToSchedule(
+                new SpanishGrammar(),
+                new SpanishGrammarHelper());
+        }
+    }
+}
